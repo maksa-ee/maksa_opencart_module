741bcceee004e9a66d247e6d7c959a35b169a920
cravler <sergeizz@gmail.com>
2012-02-06 15:45:30 +0200

    maksa.ee logo added

--- admin/language/english/payment/maksa_ee.php
+++ admin/language/english/payment/maksa_ee.php
@@ -5,6 +5,8 @@ $_['heading_title'] = 'Makse.ee';
 // Text
 $_['text_payment'] = 'Payment';
 $_['text_success'] = 'Success: You have modified Maksa.ee account details!';
+$_['text_maksa_ee'] = '<a href="https://maksa.ee" target="_blank" style="font-family:Vegur,Verdana; padding:2px 35px 2px 2px; font-size:20px; font-weight:bold; color:#5E6C87; background: url(view/image/payment/maksa_ee.png) no-repeat scroll right 1px transparent; border:1px solid #EEEEEE;">Maksa</a>';
+
 
 // Entry
 $_['entry_test_mode']        = 'Test Mode:';
