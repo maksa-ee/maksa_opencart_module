88e9673b687fc76c7279cc6c3d20bbc07086e202
Alex Rudakov <alexandr.rudakov@modera.net>
2012-02-07 14:22:25 +0200

    Added russian translations

---
+++ catalog/language/russian/payment/maksa_ee.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['text_title']           = 'Maksa.ee';
+$_['text_total']           = 'Shipping, Handling, Discounts & Taxes';
+$_['text_please_wait']     = 'Подождите пока ваш платеж обрабатывается...';
+$_['text_payment_success'] = 'Оплата произведена успешно!';
+$_['text_payment_failure'] = 'При оплате произошла ошибка! Платеж отменен';
+$_['text_go_to_order']     = 'Вернуться к заказу';
